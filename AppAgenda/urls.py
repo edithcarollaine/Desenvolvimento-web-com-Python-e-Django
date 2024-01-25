107cbbc727fe04d4231ae14003e849e75a77bf91
edithcarollaine <edith.carollaine12@gmail.com>
2024-01-25 18:10:59 -0300

    adicionados pages para criar novos eventos

--- AppAgenda/urls.py
+++ AppAgenda/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from AppAgenda import views
 
 urlpatterns = [
-    path('agenda/', views.lista_evento),
     path('', views.home),
+    path('agenda/', views.lista_evento),
+    path('agenda/evento', views.evento),
+    path('agenda/submit', views.submit_evento),
     path('login/', views.login_user),
     path('login/submit', views.submit_login),
     path('logout/', views.logout_user),
