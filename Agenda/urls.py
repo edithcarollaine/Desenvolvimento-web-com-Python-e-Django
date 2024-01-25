825e0396f714560ad60cb5823f768eaf06e0277f
edithcarollaine <edith.carollaine12@gmail.com>
2024-01-25 16:22:20 -0300

    Removendo arquivos idea do projeto

--- Agenda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from AppAgenda import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('AppAgenda.urls')),
-]
